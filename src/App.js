b8891f89199d3e24d8233ef17b8d3c30f700a833
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2021-11-22 12:06:40 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  CAlert,
+  CAvatar,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardImage,
+  CCardTitle,
+  CCardText,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@coreui/coreui/dist/css/coreui.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CCard style={{ width: '18rem' }}>
+        <CCardImage orientation="top" src={logo} />
+        <CCardBody>
+          <CCardText>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </CCardText>
+        </CCardBody>
+      </CCard>
+      <CCard style={{ width: '18rem' }}>
+        <CCardImage orientation="top" src={logo} />
+        <CCardBody>
+          <CCardText>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </CCardText>
+        </CCardBody>
+      </CCard>
+      <CAlert color="info">
+        A simple info alert—check it out!
+      </CAlert>
     </div>
   );
 }
