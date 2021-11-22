c895c5f25a8aa17ded1e4244eefc262c32c2ac97
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2021-11-22 12:24:52 -0500

    graph

--- src/App.js
+++ src/App.js
@@ -25,30 +25,169 @@ import {
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@coreui/coreui/dist/css/coreui.min.css'
+import {
+  CChart
+} from "@coreui/react-chartjs"
 
 function App() {
   return (
     <div className="App">
-      <CCard style={{ width: '18rem' }}>
-        <CCardImage orientation="top" src={logo} />
+      <CCard>
+        <CCardHeader component="h5">Paperplane App</CCardHeader>
         <CCardBody>
-          <CCardText>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </CCardText>
+          <CCardTitle>Data Dashboard</CCardTitle>
+          <CCardText>Headset float sessions</CCardText>
+          <CButton href="#">Refresh</CButton>
         </CCardBody>
       </CCard>
-      <CCard style={{ width: '18rem' }}>
-        <CCardImage orientation="top" src={logo} />
-        <CCardBody>
-          <CCardText>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </CCardText>
-        </CCardBody>
-      </CCard>
-      <CAlert color="info">
-        A simple info alert—check it out!
-      </CAlert>
+      <CChart
+        type="line" 
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [
+            {
+              label: "My First dataset",
+              backgroundColor: "rgba(220, 220, 220, 0.2)",
+              borderColor: "rgba(220, 220, 220, 1)",
+              pointBackgroundColor: "rgba(220, 220, 220, 1)",
+              pointBorderColor: "#fff",
+              data: [40, 20, 12, 39, 10, 40, 39, 80, 40]
+            },
+            {
+              label: "My Second dataset",
+              backgroundColor: "rgba(151, 187, 205, 0.2)",
+              borderColor: "rgba(151, 187, 205, 1)",
+              pointBackgroundColor: "rgba(151, 187, 205, 1)",
+              pointBorderColor: "#fff",
+              data: [50, 12, 28, 29, 7, 25, 12, 70, 60]
+            },
+          ],
+        }}
+      />
+      <CChart
+        type="bar"
+        data={{
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+            {
+              label: 'GitHub Commits',
+              backgroundColor: '#f87979',
+              data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
+            },
+          ],
+        }}
+        labels="months"
+      />
+      <CChart
+        type="doughnut"
+        data={{
+          labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+          datasets: [
+            {
+              backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+              data: [40, 20, 80, 10],
+            },
+          ],
+        }}
+      />
+      <CChart 
+        type="radar"
+        data={{
+          labels: [
+            'Eating',
+            'Drinking',
+            'Sleeping',
+            'Designing',
+            'Coding',
+            'Cycling',
+            'Running',
+          ],
+          datasets: [
+            {
+              label: 'My First dataset',
+              backgroundColor: 'rgba(220, 220, 220, 0.2)',
+              borderColor: 'rgba(220, 220, 220, 1)',
+              pointBackgroundColor: 'rgba(220, 220, 220, 1)',
+              pointBorderColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(220, 220, 220, 1)',
+              data: [65, 59, 90, 81, 56, 55, 40],
+            },
+            {
+              label: 'My Second dataset',
+              backgroundColor: 'rgba(151, 187, 205, 0.2)',
+              borderColor: 'rgba(151, 187, 205, 1)',
+              pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+              pointBorderColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(151, 187, 205, 1)',
+              data: [28, 48, 40, 19, 96, 27, 100],
+            },
+          ],
+        }}
+      />
+      <CChart
+        type="polarArea"
+        data={{
+          labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
+          datasets: [
+            {
+              data: [11, 16, 7, 3, 14],
+              backgroundColor: ['#FF6384', '#4BC0C0', '#FFCE56', '#E7E9ED', '#36A2EB'],
+            },
+          ],
+        }}
+      />
+      <CChart
+        type="bubble"
+        data={{
+          datasets: [{
+            label: 'First Dataset',
+            data: [{
+              x: 20,
+              y: 30,
+              r: 15
+            }, {
+              x: 40,
+              y: 10,
+              r: 10
+            }],
+            backgroundColor: 'rgb(255, 99, 132)'
+          }]
+        }}
+      />
+      <CChart
+        type="scatter"
+        data={{
+          datasets: [{
+            label: 'Scatter Dataset',
+            data: [{
+              x: -10,
+              y: 0
+            }, {
+              x: 0,
+              y: 10
+            }, {
+              x: 10,
+              y: 5
+            }, {
+              x: 0.5,
+              y: 5.5
+            }],
+            backgroundColor: 'rgb(255, 99, 132)'
+          }],
+        }}
+        options={{
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom'
+            }
+          }
+        }}
+      />
     </div>
+    
   );
 }
 
