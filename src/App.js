31cbbfd5072b4a7e893b87b08dbada89b2043ee0
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2021-11-30 09:18:08 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -34,67 +34,15 @@ function App() {
   }
 
   return (
-      <CContainer className="App">
-        <CCard>
-          <CCardHeader component="h5">Data Dashboard</CCardHeader>
-          <CCardBody>
-            <CCardTitle>Paperplane Float</CCardTitle>
-            <CCardText>Headset sessions</CCardText>
-          </CCardBody>
-        </CCard>
-        {users && users.map(user => <HeadsetList key={user.id} data={user} />)}
-        <CChart
-          type="line" 
-          data={{
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-              {
-                label: "My First dataset",
-                backgroundColor: "rgba(220, 220, 220, 0.2)",
-                borderColor: "rgba(220, 220, 220, 1)",
-                pointBackgroundColor: "rgba(220, 220, 220, 1)",
-                pointBorderColor: "#fff",
-                data: [40, 20, 12, 39, 10, 40, 39, 80, 40]
-              },
-              {
-                label: "My Second dataset",
-                backgroundColor: "rgba(151, 187, 205, 0.2)",
-                borderColor: "rgba(151, 187, 205, 1)",
-                pointBackgroundColor: "rgba(151, 187, 205, 1)",
-                pointBorderColor: "#fff",
-                data: [50, 12, 28, 29, 7, 25, 12, 70, 60]
-              },
-            ],
-          }}
-        />
-        <CChart
-          type="bar"
-          data={{
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-              {
-                label: 'GitHub Commits',
-                backgroundColor: '#f87979',
-                data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
-              },
-            ],
-          }}
-          labels="months"
-        />
-        <CChart
-          type="doughnut"
-          data={{
-            labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
-            datasets: [
-              {
-                backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-                data: [40, 20, 80, 10],
-              },
-            ],
-          }}
-        />
-      </CContainer>
-  );
+    <CContainer className="App">
+      <CCard>
+        <CCardBody>
+          <CCardTitle>Paperplane Float</CCardTitle>
+        </CCardBody>
+      </CCard>
+      {users && users.map(user => <HeadsetList key={user.id} data={user} />)}
+    </CContainer>
+  )
 }
 
-export default App;
+export default App
